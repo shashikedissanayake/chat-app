e374ce86cb87eb6b2552eeb9814a5ac2d3cdce8c
Shashika Chathuranga <shashikedissanayake@gmail.com>
2021-09-28 17:53:35 +0530

    Implemented view notify
    - Implemented message viewed notify to other party in backend
    - Implemented message change color according to message view or not in
    front-end

---
+++ server/src/constants/messageStatus.ts
@@ -0,0 +1,10 @@
+export enum MessageStatus {
+    SENT = 0,
+    DELIVERED = 1,
+    VIEWED = 2,
+}
+
+export enum MessageTypes {
+    CHAT_MESSAGES = 0,
+    STATUS_UPDATES = 1,
+}
--- server/src/models/messagesModel.ts
+++ server/src/models/messagesModel.ts
@@ -1,7 +1,11 @@
+import { MessageStatus, MessageTypes } from '../constants/messageStatus';
+
 export interface Message {
     id: string;
     to: string;
     from: string;
     timestamp: string;
-    message: string;
+    message?: string;
+    status: MessageStatus;
+    messageType: MessageTypes;
 }
